d8a56ccff8634bb4689ed1ec1a5827c29fc6246b
Matt Webb <mattrw89@gmail.com>
2013-11-09 16:19:48 -0600

    a

--- app/controllers/doorkeeper/tokens_controller.rb
+++ app/controllers/doorkeeper/tokens_controller.rb
@@ -5,7 +5,10 @@ module Doorkeeper
     def create
       response = strategy.authorize
       #so that devise #trackable works
-      current_resource_owner.update_tracked_fields!(request)
+      current_resource_owner.sign_in_count ||= 0
+      current_resource_owner.sign_in_count += 1
+      current_resource_owner.save(:validate => false)
+
       self.headers.merge! response.headers
       self.response_body = response.body.to_json
       self.status        = response.status
